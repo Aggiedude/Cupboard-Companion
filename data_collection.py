183b45bfff30479eba9e8d1bf9bc1f2e7ff8070b
Amanda Schlaudt <amanda_schlaudt@yahoo.com>
2016-03-30 14:52:36 -0500

    Hitting the API
    Trying to get the response decoded so we can get individual recipes.

--- data_collection.py
+++ data_collection.py
@@ -15,28 +15,37 @@ def replace_chars(query):
 	query = string.replace(query, '[', '%5b')
 	query = string.replace(query, ']', '%5d')
 	return query
+	
+def process_request(url):
+	request = urllib2.Request(url)
+	requestOpener = urllib2.build_opener()
+	response = requestOpener.open(request)
+	results = json.load(response)
+	return results
 
+def evaluate_recipe(recipeID, yummlyCredentials):
+	recipeURL = 'http://api.yummly.com/v1/api/recipe/%s?%s' % (recipeID, yummlyCredentials)
+	
+	
+	
 def main():
 	#credentials structure: _app_id=app-id&_app_key=app-key
-	yummlyAppID = " " #appID goes here
-	yummlyAppKey = " " #appKey goes here
+	yummlyAppID = "1a10b2e0"
+	yummlyAppKey = "72595b3dee46471a8a93caa35baf8ef1"
 	yummlyCredentials = '%s=%s&%s=%s' % ('_app_id', yummlyAppID, '_app_key', yummlyAppKey)
 	
 	query = "'fish tacos'"
 	urlQuery = 'q=' + replace_chars(query)
-	
 	searchParamenters = urlQuery
 	
 	url = 'http://api.yummly.com/v1/api/recipes?%s&%s' % (yummlyCredentials, searchParamenters)
+	results = process_request(url)
 	
-	print url
+	decoded = re.sub(r'\W+', ' ', results)
 	
-	request = urllib2.Request(url)
-	requestOpener = urllib2.build_opener()
-	response = requestOpener.open(request)
 	
-	results = json.load(response)
-	print results
+	
+	print decoded
 	
 if __name__ == '__main__':
 	main()
