07aee729b664ca9b8e3a98c7705796d9cb193ad2
Andrew Chepey <achepey@gmail.com>
2016-04-11 16:23:47 -0500

    Modified and added directions to simplicity algorithm

--- data_collection.py
+++ data_collection.py
@@ -1,8 +1,11 @@
 import urllib2, json, string, base64, re, sys
 from bs4 import BeautifulSoup
+from textstat.textstat import textstat
 
 scored_recipes = []
+ingrList = []
 
+# replaces common characters in URL queries with appropriately formatted characters
 def replace_chars(query):
 	query = string.replace(query, "'", '%27')
 	query = string.replace(query, '"', '%27')
@@ -14,7 +17,8 @@ def replace_chars(query):
 	query = string.replace(query, '[', '%5b')
 	query = string.replace(query, ']', '%5d')
 	return query
-	
+
+# uses the urllib function to grab the response to a HTTP GET request
 def process_request(url):
 	request = urllib2.Request(url)
 	requestOpener = urllib2.build_opener()
@@ -22,11 +26,26 @@ def process_request(url):
 	results = json.load(response)
 	return results
 
+# returns the approximate grade-level needed to read the text
+# used the formula obtained from wikipedia
+def flesch_kincaid_score(text):
+	sylCount = textstat.syllable_count(text)
+	wordCount = len(text.split())
+	sentenceCount = textstat.sentence_count(text)
+
+	print "Syl count - %s, word count - %s, sentenceCount - %s " % (sylCount,wordCount,sentenceCount)
+
+	return (0.39*(wordCount/sentenceCount)+11.8*(sylCount/wordCount) - 15.59)
+
+def num_on_hand
+
+# Calls the Yummly API to get details of a specific recipe
 def get_recipe(recipeID, yummlyCredentials):
 	recipeURL = 'http://api.yummly.com/v1/api/recipe/%s?%s' % (recipeID, yummlyCredentials)
 	recipeInfo = process_request(recipeURL)
 	return recipeInfo
 
+# Scrapes the source HTML page gathered from the yummly api for recipe directions 
 # currently only works with recipes taken from foodnetwork.com
 def get_directions(url):
 	page = urllib2.urlopen(url)
@@ -41,35 +60,57 @@ def get_directions(url):
 		directions += sibling.get_text() + "\n"
 
 	return directions
-	
+
+# main algorithm for project
+# Extracts all necessary data from the yummly api, and calls the simplicity algorithm
 def evaluate_recipe(recipe):
 	recipeName = recipe['name'].encode('ascii','ignore')
-	recipeTime = str(recipe['totalTimeInSeconds']).encode('ascii','ignore')
-	numIngredients = str(len(recipe['ingredientLines'])).encode('ascii','ignore')
+	print "evaluating %s" % recipeName
+	
+	prepTime = 0
+	if 'prepTimeInSeconds' in recipe:
+		prepTime = recipe['prepTimeInSeconds']
+
+	cookTime = 0
+	if 'cookTimeInSeconds' in recipe:
+		cookTime = recipe['cookTimeInSeconds']
+	else:
+		if prepTime == 0: # API returned non-specific cooking time
+			cookTime = recipe['totalTimeInSeconds'] # will use total time as cook time in algorithm instead
+
+	numIngredients = len(recipe['ingredientLines'])
 	flavors = recipe['flavors']
+
 	sourceDict = recipe['source']
 	sourceName = sourceDict['sourceDisplayName'].encode('ascii','ignore')
-	print sourceName
-	directions = ""
+	directionsText = ""
 
 	# currently, only Food Network sources work. 
 	if "Food Network" in sourceName:
-		directions = get_directions(sourceDict['sourceRecipeUrl'])
-		print directions
+		directionsText = get_directions(sourceDict['sourceRecipeUrl'])
+		print directionsText
 
-	scored_recipes.append((recipeName,evaluate_simplicity(numIngredients,recipeTime)))
+	if directionsText:
+		simplicityScore = evaluate_simplicity(numIngredients,prepTime,cookTime,directionsText)
+		scored_recipes.append((recipeName,simplicityScore))
 
-	importantInfo = "The recipe " + recipeName + " has " + numIngredients + " ingredients and takes " + recipeTime + " seconds to make."
-	return importantInfo 
+# Assigns a score to a recipe based upon a number of factors 
+def evaluate_simplicity(numIng, prepTime, cookTime, directionsText):
+	numIngConst = 0.6
+	prepConst = 0.002
+	cookConst = 0.001
+	fkConst = 0.5
+	stepsConst = 0.3
+	notOnHandConst = 2.0
 
-def evaluate_simplicity(numIng, recipeTime):
-	alpha = 0.6
-	beta = 0.001
+	fkScore = flesch_kincaid_score(directionsText)
+	numSteps = len([s.strip() for s in directionsText.splitlines()])
 
-	return alpha*int(numIng) + beta*int(recipeTime)
 
-def printResults(query):
-	print 'The simplest recipes for the query ' + query + ' is the following:'
+	return numIngConst*numIng + prepConst*prepTime + cookConst*cookTime + fkConst*fkScore + stepsConst*numSteps
+
+def printResults(ingList):
+	print 'The simplest recipes for the ingredients ' + str(ingList) + ' is the following:'
 	i = 0
 	while i < len(scored_recipes):
 		print str(i+1) + '. ' + str(scored_recipes[i])
@@ -81,12 +122,10 @@ def main(argv):
 	yummlyAppKey = "72595b3dee46471a8a93caa35baf8ef1"
 	yummlyCredentials = '%s=%s&%s=%s' % ('_app_id', yummlyAppID, '_app_key', yummlyAppKey)
 
-	#queryTextFile = argv[0]
-	#f = open(queryTextFile)
-	#query = f.read().lower()
-	query = ''
-	
-	query = "'%s'" % query
+	queryTextFile = argv[0]
+	f = open(queryTextFile)
+	ingrList = f.read().lower().split()
+
 	#urlQuery = 'q=' + replace_chars(query)
 	urlQuery = "q=Food+Network"
 	allowedIngredients = ['chicken']
@@ -107,16 +146,13 @@ def main(argv):
 		#print item['id']
 		completeRecipes.append(get_recipe(item['id'], yummlyCredentials))
 		
-	recipeDetails = []
+	# evaluate the simplicity for every recipe gathered
 	for item in completeRecipes:
-		recipeDetails.append(evaluate_recipe(item))
-	
-	for detail in recipeDetails:
-		print detail
+		evaluate_recipe(item)
 	
 	scored_recipes.sort(key=lambda number: number[-1])
 
-	printResults(query)
+	printResults(ingrList)
 	
 	#print results
 	
